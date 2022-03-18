5af511c0848fef5a0e15f4897873f5a55f10a940
Ricky <drickyriverab@gmail.com>
2022-03-18 16:09:23 +0100

    using phoenix presence now

---
+++ Server/lib/picture_this_web/channels/presence.ex
@@ -0,0 +1,10 @@
+defmodule PictureThisWeb.Presence do
+  @moduledoc """
+  Provides presence tracking to channels and processes.
+
+  See the [`Phoenix.Presence`](https://hexdocs.pm/phoenix/Phoenix.Presence.html)
+  docs for more details.
+  """
+  use Phoenix.Presence, otp_app: :picture_this,
+                        pubsub_server: PictureThis.PubSub
+end
