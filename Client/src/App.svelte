1e41cd3ab058b123c2af251184c7b792d69cd6a7
danfry1 <danfry@hotmail.co.za>
2022-03-18 17:26:20 +0100

    basic canvas

--- Client/src/App.svelte
+++ Client/src/App.svelte
@@ -1,47 +1,46 @@
 <script lang="ts">
-  let name = ['gaut', 'dan', 'rick'];
-  let index = 0;
-  function nextPerson() {
-    if (index === 2) {
-      index = 0;
-    } else {
-      index = index + 1;
-    }
+  const canvas = document.createElement('canvas');
+
+  document.body.appendChild(canvas);
+
+  document.body.style.margin = '0';
+
+  canvas.style.position = 'fixed';
+  var ctx = canvas.getContext('2d');
+  resize();
+  var pos = { x: 0, y: 0 };
+
+  window.addEventListener('resize', resize);
+  document.addEventListener('mousemove', draw);
+  document.addEventListener('mousedown', setPosition);
+  document.addEventListener('mouseenter', setPosition);
+
+  function setPosition(e) {
+    pos.x = e.clientX - 10;
+    pos.y = e.clientY - 10;
   }
-  function prevPerson() {
-    if (index === 0) {
-      index = 2;
-    } else {
-      index = index - 1;
-    }
+
+  function resize() {
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+  }
+
+  function draw(e) {
+    if (e.buttons !== 1) return;
+
+    ctx.beginPath();
+
+    ctx.lineWidth = 5;
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = 'black';
+
+    ctx.moveTo(pos.x, pos.y);
+    setPosition(e);
+    ctx.lineTo(pos.x, pos.y);
+
+    ctx.stroke();
   }
 </script>
 
-<main>
-  <h1>Hello {name[index]}!</h1>
-  <button on:click={prevPerson}>Prev</button>
-
-  <button on:click={nextPerson}>Next</button>
-</main>
-
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
 </style>
