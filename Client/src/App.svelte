ff1fab7e330706f04f8d6e598bb42e446096a91c
danfry1 <danfry@hotmail.co.za>
2022-03-18 12:20:19 +0100

    Initial Commit

---
+++ Client/src/App.svelte
@@ -0,0 +1,47 @@
+<script>
+  let name = ['gaut', 'dan', 'rick'];
+  let index = 0;
+  function nextPerson() {
+    if (index === 2) {
+      index = 0;
+    } else {
+      index = index + 1;
+    }
+  }
+  function prevPerson() {
+    if (index === 0) {
+      index = 2;
+    } else {
+      index = index - 1;
+    }
+  }
+</script>
+
+<main>
+  <h1>Hello {name[index]}!</h1>
+  <button on:click={prevPerson}>Prev</button>
+
+  <button on:click={nextPerson}>Next</button>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
