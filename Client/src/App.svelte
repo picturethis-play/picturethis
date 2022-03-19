e9d34006018c7daece75f28abb52a9a79479f871
Gaute Rongve <gaute.rongve@gmail.com>
2022-03-19 16:46:47 +0100

    FEAT: Made Chat component and imported it to App.svelte

--- Client/src/App.svelte
+++ Client/src/App.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import Canvas from './components/canvas.svelte';
   import Header from './components/Header.svelte';
+  import Chat from './components/Chat.svelte';
 </script>
 
 <div class="app">
   <Header />
   <Canvas />
+  <Chat />
 </div>
 
 <style>
