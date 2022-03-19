b2ecf558d41d2f697833ccdc62aa36b34a5d17d4
danfry1 <danfry@hotmail.co.za>
2022-03-19 17:16:37 +0100

    Landing page and GamePage

---
+++ Client/src/components/ProgressBar.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { writable } from 'svelte/store';
+
+  const time = 1;
+  const progress = writable(time);
+  const coundown = () => {
+    if (time == 0) return;
+    setInterval(() => (time = time - 0.1), 1000);
+  };
+  $: progress.set(time);
+</script>
+
+<progress value={$progress} />
+
+<!-- <button on:click={() => progress.set(0)}> 0% </button>
+
+<button on:click={() => progress.set(0.25)}> 25% </button>
+
+<button on:click={() => progress.set(0.5)}> 50% </button> -->
+
+<!-- <button on:click={() => progress.set(0.75)}> 75% </button> -->
+
+<!-- <button on:click={() => progress.set(1)}> 100% </button> -->
+<button on:click={() => coundown()}> 100% </button>
+
+<style>
+  progress {
+    display: block;
+    width: 100%;
+  }
+</style>
