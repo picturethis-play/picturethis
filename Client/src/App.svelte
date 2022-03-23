c34796355ad4232f1a32a2de50ea1027f947bc8a
Gaute Rongve <gaute.rongve@gmail.com>
2022-03-23 15:43:50 +0100

    FIX: index.svelte => tailwind

--- Client/src/App.svelte
+++ Client/src/App.svelte
@@ -3,9 +3,5 @@
 </script>
 
 <main>
-  <!-- <h1 class="text-6xl font-normal leading-normal mt-0 mb-2 text-pink-800">Hello Gaudi!</h1>
-
-  <h1 class="text-blue-700">Hello!</h1>
-  <h2>Test!!</h2> -->
   <Router />
 </main>
