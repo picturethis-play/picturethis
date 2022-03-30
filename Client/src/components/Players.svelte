7ff453870b17e453d1ae9af5fcb2729258550168
danfry1 <danfry@hotmail.co.za>
2022-03-30 15:45:20 +0200

    filtering by points

--- Client/src/components/Players.svelte
+++ Client/src/components/Players.svelte
@@ -12,21 +12,22 @@
     playerz = JSON.parse(sessionStorage.getItem('players'));
   });
   socket.on('updateStores', () => {
-    playerz = JSON.parse(sessionStorage.getItem('players'));
+    let playerx = JSON.parse(sessionStorage.getItem('players'));
+    playerz = playerx.sort((a, b) => b.points - a.points);
   });
 </script>
 
-  <div
-    class="border-2 rounded-md border-secondary shadow-69xl shadow-secondary overflow-y-auto xl:h-xl-jr lg:h-rr lg:mb-2 sm:h-36 sm:w-96 md:h-48 md:w-df mw-72 w-48 bg-neutral p-3"
-  >
-    {#if playerz.length > 0}
-      {#each playerz as user}
-        <h2 class="text-primary">{user.points}</h2>
-        {#if user.id === JSON.parse(drawer).id}
-          <p class="text-2x text-primary">{user.name} ✍️</p>
-        {:else}
-          <p class="text-2x text-accent">{user.name}</p>
-        {/if}
-      {/each}
-    {/if}
-  </div>
+<div
+  class="border-2 rounded-md border-secondary shadow-69xl shadow-secondary overflow-y-auto xl:h-xl-jr lg:h-rr lg:mb-2 sm:h-36 sm:w-96 md:h-48 md:w-df mw-72 w-48 bg-neutral p-3"
+>
+  {#if playerz.length > 0}
+    {#each playerz as user}
+      <h2 class="text-primary">{user.points}</h2>
+      {#if user.id === JSON.parse(drawer).id}
+        <p class="text-2x text-primary">{user.name} ✍️</p>
+      {:else}
+        <p class="text-2x text-accent">{user.name}</p>
+      {/if}
+    {/each}
+  {/if}
+</div>
