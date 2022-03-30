7cf72dc444230d586a78671c7aadd22552118385
Ricky <drickyriverab@gmail.com>
2022-03-30 11:30:09 +0200

    game works now 99 percent done

---
+++ Client/src/components/GameEndModal.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { navigate } from 'svelte-routing';
+  function navigation() {
+    navigate(`/`, { replace: true });
+  }
+</script>
+
+<input type="checkbox" id="my-modal-5" class="modal-toggle" checked />
+<div class="modal">
+  <div class="modal-box w-11/12 max-w-5xl">
+    <h3 class="font-bold text-lg">Congratulations random Interner user!</h3>
+    <p class="py-4">
+      You've been selected for a chance to get one year of subscription to use Wikipedia for free!
+    </p>
+    <div class="modal-action">
+      <label for="my-modal-5" class="btn" on:click={navigation}>Start another game :-)</label>
+    </div>
+  </div>
+</div>
--- Client/src/stores/gameStates.js
+++ Client/src/stores/gameStates.js
@@ -1,11 +1,11 @@
 import { writable } from 'svelte/store';
 
-export const timer = writable(10);
+export const timer = writable(50);
 
 export const drawerIndex = writable(0);
 export const players = writable(['gaute', 'dan', 'ricky']);
 export const gameRound = writable(0);
 export const waitingTime = writable(10);
-export const roundTime = writable(10);
-export const numberOfRounds = writable(3);
+export const roundTime = writable(50);
+export const numberOfRounds = writable(2);
 export const lengthOfSecretWord = writable([]);
