a58165ffc91af077135d6df0763e14f05565c1a1
Ricky <drickyriverab@gmail.com>
2022-07-12 17:24:26 -0700

    updated landing page to three.js

--- Client/src/pages/LandingPage.svelte
+++ Client/src/pages/LandingPage.svelte
@@ -1,14 +1,15 @@
-<script lang="ts">
-  import { navigate } from 'svelte-routing';
-  import { fade, fly } from 'svelte/transition';
-  import { onMount, getContext } from 'svelte';
-  import { game } from '../stores/chat-stores'
-  import { themeChange } from 'theme-change';
-  let penColor;
-  let canvas;
-  let ctx;
-  let stroke;
-  let lineWidth = 2;
+<script>
+import { navigate } from 'svelte-routing';
+import { onMount, getContext } from 'svelte';
+import { game } from '../stores/chat-stores';
+import { themeChange } from 'theme-change';
+import * as THREE from 'three';
+import { OrbitControls } from '../../public/OrbitControls';
+import { FontLoader } from '../../public/FontLoader';
+import { TextGeometry } from '../../public/TextGeometry';
+
+
+/////////////////////////////////////////////////////////////////////////////////////
   const { Socket } = getContext('connect');
   const socket = Socket();
   let room = '';
@@ -21,67 +22,144 @@
     return navigate(`/waitingRoom`, { replace: true });
   }
   onMount(() => {
-     themeChange(false);
+    themeChange(false);
+  
+  const canvas = document.querySelector('canvas.webgl')
+  
+  //lol it made me do this for some reason🤷🏽‍♂️
+  canvas.style.position = 'absolute';
+  
+  // Scene
+  const scene = new THREE.Scene()
+  
+  
+  /**
+   * Textures
+   */
+  const textureLoader = new THREE.TextureLoader()
+  
+  const matcapTexture = textureLoader.load('/textures/matcaps/1.png');
+  const matcapTexture2 = textureLoader.load('/textures/matcaps/3.png');
+  
+  const fontLoader = new FontLoader();
+  const donutMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
+  const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture2 });
+  
+  fontLoader.load('/fonts/helvetiker_regular.typeface.json', (font) => {
+      const textGeometry = new TextGeometry('Welcome to PictureThis :-)', {
+          font,
+          size: 0.3,
+          height: 0.2,
+          curveSegments: 5,
+          bevelEnabled: true,
+          bevelThickness: 0.03,
+          bevelSize: 0.02,
+          bevelOffset: 0,
+          bevelSegments: 4
+      })
+    
+  
+      textGeometry.center();
+     
+      const text = new THREE.Mesh(textGeometry, material);
+     
+      scene.add(text);
+     
+  })
+  const torusGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
+  for (let i = 0; i < 333; i++) {
+      const torus = new THREE.Mesh(torusGeometry, donutMaterial);
+      torus.position.x = (Math.random() - 0.5) * 10;
+      torus.position.y = (Math.random() - 0.5) * 10;
+      torus.position.z = (Math.random() - 0.5) * 10;
+  
+      torus.rotation.x = Math.random() * Math.PI
+      torus.rotation.y = Math.random() * Math.PI
+      const scaler = Math.random();
+      torus.scale.x = scaler;
+      torus.scale.y = scaler;
+      torus.scale.z = scaler;
+  
+      scene.add(torus);
+  };
+  
+  /**
+   * Responsive Canvas
+   */
+  const sizes = {
+      width: window.innerWidth,
+      height: window.innerHeight
+  }
+  window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+    
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+    
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  })
+  
+  /**
+   * Renderer
+   */
+  const renderer = new THREE.WebGLRenderer({
+      canvas: canvas
+  })
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  /**
+   * Camera
+   */
+  const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+  camera.position.x = 1
+  camera.position.y = 1
+  camera.position.z = 2
+  scene.add(camera)
+  
+  // Controls
+  const controls = new OrbitControls(camera, canvas)
+  controls.enableDamping = true
+  
+
+
+  
+  
+  /**
+   * Animation
+   */
+  const clock = new THREE.Clock()
+  
+  const tick = () => {
+      const elapsedTime = clock.getElapsedTime()
+  
+      // Update controls
+      controls.update()
+      // Render
+      renderer.render(scene, camera)
+      torusGeometry.rotateX(0.02)
+      torusGeometry.rotateY(0.02)
+
+      if(camera.position.z < 4){
+        camera.position.z += 0.01
+      }
+  
+      window.requestAnimationFrame(tick)
+  }
+  tick()
     // 👆 false parameter is required for svelte
-    canvas = document.getElementById('theCanvas');
-    ctx = canvas.getContext('2d');
-    canvas.height = 500;
-    canvas.width = 800;
   });
-
-  function getMousePosition(e) {
-    var mouseX = ((e.offsetX * canvas.width) / canvas.clientWidth) | 0;
-    var mouseY = ((e.offsetY * canvas.height) / canvas.clientHeight) | 0;
-    return { x: mouseX, y: mouseY };
-  }
-  function randomColor() {
-    const colors = ['blue', 'yellow', 'red', 'purple', 'orange', 'pink', 'green'];
-    const random = Math.floor(Math.random() * colors.length);
-    stroke = colors[random];
-  }
-  function handleDraw(e) {
-    const mousePosition = getMousePosition(e);
-    draw(mousePosition, lineWidth, penColor);
-  }
-  function draw(mousePosition, lineWidth, penColor) {
-    ctx.lineWidth = lineWidth;
-    ctx.strokeStyle = penColor;
-    ctx.lineCap = 'round';
-    ctx.strokeStyle = `${stroke}`;
-    ctx.lineTo(mousePosition.x, mousePosition.y);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(mousePosition.x, mousePosition.y);
-  }
+ 
 </script>
-
-<div id="container" class="cursor-pointer overflow-hidden">
+  <div class="cursor-pointer" >
+<canvas class="webgl"  ></canvas>
+</div>
+<div id="container" class="overflow-hidden"> 
   <div id="overlay">
-    <div class=" flex items-center justify-center">
-      <canvas class="" id="theCanvas" on:mousemove={handleDraw} on:mousedown={randomColor}  on:touchmove={handleDraw} on:touchstart={randomColor}/>
-      <div class="flex items-center font-logo text-69xl sm:text-5xl md:text-10xl text-accent">
-        {#each 'picture' as char, i}
-          <p
-            class="animate-bouncer hover:animate-ping"
-            in:fade={{ delay: 1000 + i * 150, duration: 1500 }}
-            out:fly={{ y: -20, duration: 1000 }}
-          >
-            {char}
-          </p>
-        {/each}
-      </div>
-      <div class="flex items-center font-logo text-69xl sm:text-5xl md:text-10xl text-secondary">
-        {#each 'this' as char, i}
-          <p
-            class="animate-bouncey hover:animate-ping"
-            in:fade={{ delay: 2000 + i * 150, duration: 1800 }}
-            out:fly={{ y: -20, duration: 2000 }}
-          >
-            {char}
-          </p>
-        {/each}
-      </div>
-    </div>
     <div class="flex flex-col items-center justify-center font-logo gap-8" id="start">
       <input
         class="input input-ghost input-lg text-2xl "
@@ -93,12 +171,13 @@
       />
       <button class="btn btn-outline text-2xl" on:click={enterIfYouDare}>Join or create game</button
       >
-      <input
-      type="checkbox"
-      class="toggle mt-1"
-      data-toggle-theme="emerald,dracula"
-      data-act-class="ACTIVECLASS"
-    />🌚/🌞
+             <input
+        type="checkbox"
+        class="toggle mt-1"
+        data-toggle-theme="emerald,dracula"
+        data-act-class="ACTIVECLASS"
+      />🌚/🌞
+
     </div>
   </div>
 </div>
